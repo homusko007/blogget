c86b153fe86dc6837f217acf814d2630b9e3d6e5
Natalia Homusko <avgust_net85@mail.ru>
2022-12-01 13:39:57 +0300

    start

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+
+function App() {
+  return (
+    
+      <header className="App-header">
+        
+         <p>Hello React</p> 
+
+      </header>
+    
+  );
+}
+
+export default App;
